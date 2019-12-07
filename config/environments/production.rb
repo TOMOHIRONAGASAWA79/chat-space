8e6c4c40e3801dba8a66b343d715f4844353e22b
TOMOHIRONAGASAWA79 <tomohiro.nagasawa79@gmail.com>
2019-12-08 02:45:56 +0900

    自動デプロイの編集

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,3 +84,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+ server '13.230.218.104', user: 'ec2-user', roles: %w{app db web}
